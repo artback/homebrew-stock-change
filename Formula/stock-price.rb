f73bb1680d8695b3a5e394c5f4398d216c614133
Jonathan <jonathan_artback@icloud.com>
2026-02-13 08:58:17 +0100

    Bump formula to 0.1.6 and use standard install

--- Formula/stock-price.rb
+++ Formula/stock-price.rb
@@ -4,16 +4,14 @@ class StockPrice < Formula
   desc "CLI to track stock prices and dividends"
   homepage "https://github.com/artback/stock-change"
   url "https://github.com/artback/stock-change.git", branch: "main"
-  version "0.1.5"
+  version "0.1.6"
 
   depends_on "python@3.12"
 
   def install
     venv = virtualenv_create(libexec, "python3.12")
-    venv.pip_install "yfinance"
-    venv.pip_install "rich"
-    venv.pip_install "PyYAML"
-    venv.pip_install "python-dotenv"
+    # This will install the package AND all its dependencies (numpy, pandas, etc.)
+    # from the pyproject.toml requirements.
     venv.pip_install_and_link buildpath
   end
 
